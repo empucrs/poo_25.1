8700616861ab85b08f800a64465c8b96069c2938
Edson Ifarraguirre Moreno <10083346@facin.portoalegre.pucrsnet.br>
2025-03-12 09:40:13 -0300

    Desenvolvimento parcial do exercicio do cofrinho

---
+++ Aula_0312/Cofrinho/src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) {
+        Cofrinho c = new Cofrinho();
+
+        Moeda m = new Moeda(NomeMoeda.UmReal);
+        c.insere(m);
+        m = new Moeda(NomeMoeda.Cinquenta);
+        c.insere(m);
+        m = new Moeda(NomeMoeda.VinteCinco);
+        c.insere(m);
+        System.out.println(c);
+
+        System.out.println("Removendo 1 moeda");
+        m=c.retira();
+        System.out.println("==> Moeda retirada"+m.toString());
+        System.out.println("Lista moedas restantes");
+        System.out.println(c);
+
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.UmReal)+ " moedas de um real");
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.Cinquenta)+ " moedas de cinquenta centavos");
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.VinteCinco)+ " moedas de vinte e cinco centavos");
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.Dez)+ " moedas de dez centavos");
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.Cinco)+ " moedas de cinco centavos");
+        System.out.println("Há "+c.getQtdadeMoedasTipo(NomeMoeda.Um)+ " moedas de um centavo");
+
+    }
+}
---
+++ Aula_0312/Cofrinho/src/Cofrinho.java
@@ -0,0 +1,53 @@
+public class Cofrinho {
+
+    private Moeda [] listaDeMoedas;
+    private int posicaoLivre;
+
+    public Cofrinho(){
+        this.listaDeMoedas = new Moeda[10];
+        this.posicaoLivre=0;
+    }
+
+    public boolean insere(Moeda moeda){
+        if(posicaoLivre>=listaDeMoedas.length)
+            return false;
+        listaDeMoedas[posicaoLivre]=moeda;
+        posicaoLivre++;
+        return true;
+    }
+
+    public String toString(){
+        StringBuilder result = new StringBuilder();
+
+        result.append("Lista de moedas:\n");
+        for(int i=0; i<posicaoLivre; i++)
+          result.append("  "+listaDeMoedas[i].toString()+"\n");
+
+        return result.toString();
+    }
+
+    public Moeda retira(){
+        if(posicaoLivre==0)
+            return null;
+
+        posicaoLivre--;
+        return listaDeMoedas[posicaoLivre];
+
+    }
+
+    public int getQtdadeMoedas(){
+        return posicaoLivre;
+    }
+
+    public int getQtdadeMoedasTipo(NomeMoeda nomeMoeda){
+        int qtde=0;
+
+        for(int i=0; i<posicaoLivre; i++)
+          if(listaDeMoedas[i].getNomeMoeda()==nomeMoeda)
+            qtde++;
+
+        return qtde;
+    }
+
+    
+}
